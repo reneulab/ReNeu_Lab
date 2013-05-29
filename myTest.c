481fa281a0f77ba89222f30f18ecd1b31f30d811
RachSmith <RachSmith@utexas.edu>
2013-05-29 17:13:15 -0500

    Add initNTCAN and closeNTCAN

--- myTest.c
+++ myTest.c
@@ -26,6 +26,21 @@
 #define CAN_FORMAT_ERROR		20
 #define CAN_FORMAT_EVENT		21
 
+typedef struct
+{
+	uint32_t 	baud;
+	uint32_t	flags;
+	NTCAN_HANDLE	handle;
+        int32_t		ID;
+	int32_t		net;
+        int32_t		rxSize;
+	int32_t		rxTime;
+	int32_t		txSize;
+	int32_t		txTime;
+	CMSG		*obj;
+} NTCAN;
+
+
 /****************************************************************/
 /*             Function: errorCheck 				*/
 /*    command is one of the funtions in ntcan.h                 */
@@ -209,6 +224,87 @@ int32_t errorCheck(NTCAN_HANDLE handle, int32_t command,
          ret_val = 0xFF; break;
    }
    return ret_val; 
+}
+
+/***********************************************************/
+NTCAN* initNTCAN(uint32_t baud, uint32_t flags,
+		 int32_t ID, int32_t net,
+		 int32_t rxSize, int32_t rxTime,
+		 int32_t txSize, int32_t txTime)
+{
+   NTCAN	*myNTCAN;
+   NTCAN_HANDLE	handle;
+   int32_t 	result; 
+/* Open can */  
+   result = canOpen(net,flags,txSize,rxSize,
+                    txTime,rxTime,&handle);
+   if(errorCheck(handle,CAN_OPEN,result) != 0);  
+      { return NULL; } 
+   printf("canOpen() success\n");
+/* Setting baudrate */   
+   result = canSetBaudrate(handle,baud); 
+   if(errorCheck(handle,CAN_SET_BAUDRATE,result) != 0) 
+      { return NULL; }
+   result = canGetBaudrate(handle,&baud); 
+   printf("canSetBaudrate() success. Baudrate is %d\n",baud);
+/* Adding an ID */
+   do  { result = canIdAdd(handle,ID); }
+      while( errorCheck(handle,CAN_ID_ADD,result) == 2)
+   if(errorCheck(handle,CAN_ID_ADD,result) != 0)
+      { return NULL; } 
+/* Making NTCAN Device */
+   myNTCAN = malloc(sizeof(NTCAN));
+   if(myNTCAN == NULL){
+   printf("Error, not enough memory\n");
+   return NULL; }
+   myNTCAN->baud = baud;
+   myNTCAN->flag = flag;
+   myNTCAN->handle = handle;
+   myNTCAN->ID = ID;
+   myNTCAN->net = net;
+   myNTCAN->rxSize = rxSize;
+   myNTCAN->rxTime = rxTime;
+   myNTCAN->txSize = txSize;
+   myNTCAN->txTime = txTime;
+   result = canIoctl(myNTCAN->handle, NTCAN_IOCTL_TX_OBJ_CREATE,
+		     myNTCAN->obj); 
+   if(errorCheck(handle,CAN_IO_CTL,result) != 0) 
+      { return NULL; }
+   printf("Initializing sucessfull\n");
+   result = canIoctl(myNTCAN->handle,NTCAN_IOCTL_FLUSH_RX_FIFO,NULL);   
+   if(errorCheck(handle,NTCAN_IO_CTL,result) != 0) 
+      { return NULL; }
+   printf("System flushed, device ready to use\n");
+   return myNTCAN; 
+}
+
+
+/*************************************************************/
+int32_t closeNTCAN(NTCAN *myNTCAN)
+{
+   NTCAN_RESULT	result;
+/* Flushing system */  
+   result = canIoctl(myNTCAN->handle,NTCAN_IOCTL_FLUSH_RX_FIFO,NULL);   
+   if(errorCheck(handle,NTCAN_IO_CTL,result) != 0) 
+      { return -1; }
+/* Deleting NTCAN object */   
+   result = canIoctl(myNTCAN->handle, NTCAN_IOCTL_TX_OBJ_DESTROY,
+                     myNTCAN->ID); 
+   if(errorCheck(handle,NTCAN_IO_CTL,result) != 0) 
+      { return 1; }  
+   printf("Deleted NTCAN object\n");
+/* Deleting ID */      
+   do  { result = canIdDelete(myNTCAN->handle,myNTCAN->ID); }
+      while( errorCheck(myNTCAN->handle,CAN_ID_DELETE,result) == 2)
+   if(errorCheck(myNTCAN->handle,CAN_ID_DELETE,result) != 0)
+      { return 1; }
+   printf("Deleted NTCAN ID\n");
+/* Deleting NTCAN device */
+   result = canClose(myNTCAN->handle);
+   if(errorCheck(myNTCAN->handle,CAN_CLOSE,result) != 0)
+      { return 1; }
+   printf("Deleted NTCAN device. Device shutdown successfull\n");
+   return 0;
 }
 
 int32_t main(void) {
